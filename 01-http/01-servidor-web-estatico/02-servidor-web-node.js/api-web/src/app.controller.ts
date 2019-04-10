3f784cdd73eb7b0a50bd10737c8e37172479771c
jorgez93 <joluzaib@hotmail.com>
2019-04-10 16:00:21 -0500

    cambios package.json

--- 01-http/01-servidor-web-estatico/02-servidor-web-node.js/api-web/src/app.controller.ts
+++ 01-http/01-servidor-web-estatico/02-servidor-web-node.js/api-web/src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, HttpCode, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get()//metodo HTTP
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post()// metodo HTTP
+  //para importar un decorador : alt+enter
+  @HttpCode(200)
+  postHello(){
+    return 'Hola mundo en post';
+  }
 }
+/*
+@nombreDecoradorClase()//decorador ->función, se utiliza antes de instanciar clase metodo atributos o constructores , sintaxis: @nombre, este decorador es una función que se ejecuta antes de crearse la clase
+//existen decoradores para clase, atributos,parametros de funciones o en metodos
+class usuario{
+  atributoPublico;
+  private atributoPrivado;
+  protected atributoProtegido;
+  constructor(atributoPublico, stributoPrivado, atributoProtegido){
+    this.atributoPublico= atributoPublico;
+    this.atributoPrivado=atributoProtegido;
+    this.atributoProtegido=this.atributoProtegido
+
+  }
+  @MetodoA()
+  public metodoPublico(@ParametroA() a){}
+  @MetodoB()
+  public metodoPrivado(){}
+  public metodoProtegido(){}
+}
+*/
